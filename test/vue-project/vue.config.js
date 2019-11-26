c85fdd45c1e3b382021825606d3bfbcf3222f5bc
mcuking <tangjianghong@163.com>
2019-11-26 14:49:28 +0800

    feat: add vue project for test

---
+++ test/vue-project/vue.config.js
@@ -0,0 +1,17 @@
+const OfflinePackage = require('../../lib/plugin');
+
+module.exports = {
+  configureWebpack: () => {
+    return {
+      plugins: [
+        new OfflinePackage({
+          packageNameKey: 'packageId',
+          packageNameValue: 'meeting',
+          version: 1,
+          baseUrl: 'http://192.168.88.88:5000/',
+          fileTypes: ['js', 'css', 'png']
+        })
+      ]
+    };
+  }
+};
